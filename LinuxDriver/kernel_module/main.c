a83e24b34e608036b5ed62f0fa402cb33204927d
Brollof <thefish3r@gmail.com>
2021-09-02 10:41:09 +0200

    code clean

--- LinuxDriver/kernel_module/main.c
+++ LinuxDriver/kernel_module/main.c
@@ -4,10 +4,8 @@
 #include <linux/fs.h>
 #include <linux/uaccess.h>
 
-
 #define DEV_NAME "accel"
-#define LOG_PREFIX "[ACC_DRIVER] "
-
+#define LOG_PREFIX "["DEV_NAME" dev] "
 
 static int dev_open(struct inode *inode, struct file *fp);
 static ssize_t dev_read(struct file *fp, char __user *buf, size_t, loff_t *offset);
@@ -23,48 +21,47 @@ static struct file_operations fops =
   .release = dev_release
 };
 
-
 static int dev_open(struct inode *inode, struct file *fp)
 {
-  printk(KERN_INFO LOG_PREFIX "open\n");
+  printk(KERN_INFO LOG_PREFIX "Open\n");
   return 0;
 }
 
 static ssize_t dev_read(struct file *fp, char __user *buf, size_t len, loff_t *offset)
 {
-  printk(KERN_INFO LOG_PREFIX "read\n");
+  printk(KERN_INFO LOG_PREFIX "Read\n");
   return 0;
 }
 
 static ssize_t dev_write(struct file *fp, const char __user *buf, size_t len, loff_t *offset)
 {
-  printk(KERN_INFO LOG_PREFIX "write\n");
+  printk(KERN_INFO LOG_PREFIX "This device is not writable!\n");
   return -EFAULT;
 }
 
 static int dev_release(struct inode *inode, struct file *fp)
 {
-  printk(KERN_INFO LOG_PREFIX "release\n");
+  printk(KERN_INFO LOG_PREFIX "Release\n");
   return 0;
 }
 
-static int __init accDriverInit(void)
+static int __init moduleInit(void)
 {
   major_num = register_chrdev(0, DEV_NAME, &fops);
   if (major_num < 0)
   {
-    printk(KERN_INFO LOG_PREFIX "registering char dev failed! err: %d\n", major_num);
+    printk(KERN_WARNING LOG_PREFIX "Registering char dev failed! err: %d\n", major_num);
     return major_num;
   }
-  printk(KERN_INFO LOG_PREFIX "Init with major number: %d\n", major_num);
+  printk(KERN_INFO LOG_PREFIX "Registered with major number: %d\n", major_num);
   return 0;
 }
 
-static void __exit accDriverExit(void)
+static void __exit moduleExit(void)
 {
   unregister_chrdev(major_num, DEV_NAME);
-  printk(KERN_INFO LOG_PREFIX "Exit\n");
+  printk(KERN_INFO LOG_PREFIX "device exit\n");
 }
 
-module_init(accDriverInit);
-module_exit(accDriverExit);
+module_init(moduleInit);
+module_exit(moduleExit);
